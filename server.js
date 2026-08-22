588db0965605f30910c4b0c82797d3ea07ce759c
JookJookE <la5454la@gmail.com>
2026-08-22 18:07:22 +0900

    fix: v3.13.7 - 💥 브라우저 캐시 원천 차단 (express static no-cache 강제 주입)

--- server.js
+++ server.js
@@ -102,7 +102,15 @@ app.use((req, res, next) => {
   }
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), {
+  etag: false,
+  lastModified: false,
+  setHeaders: (res, path) => {
+    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
+  }
+}));
 
 // API Routes
 app.get('/api/login-history', (req, res) => {
